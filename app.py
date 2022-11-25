22c5b2b54048e42b50ffa5b971947b3417aa7ac6
AKASH YADAV <59865773+TechnologyTherapist@users.noreply.github.com>
2022-11-25 19:01:41 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+import os
+import logging
+from logging import Formatter, FileHandler
+from flask import Flask, request, jsonify
+
+from ocr import process_image
+_VERSION=1
+@app.route('/v{}/ocr'.format(_VERSION), methods=["POST"])
+def ocr():
+    try:
+        url = request.json['image_url']
+        if 'jpg' in url:
+            output = process_image(url)
+            return jsonify({"output": output})
+        else:
+            return jsonify({"error": "only .jpg files, please"})
+    except:
+        return jsonify(
+            {"error": "Did you mean to send: {'image_url': 'some_jpeg_url'}"}
+        )
